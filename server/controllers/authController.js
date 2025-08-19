1811cc79873b4c2d2b195b41dacb6df49768338f
Naveen <naveenpokhrel99@gmail.com>
2025-08-19 17:09:01 +0545

    updated the verify

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -139,3 +139,35 @@ export const sendVerifyOtp = async (req, res)=>{
     res.json({ success: false, message: error.message});
   }
 }
+
+export const verifyEmail = async (req, res) => {
+  const { userId, otp} = req.body;
+
+  if (!userId || !otp) {
+    return res.json({ success: false, message: "Missing details" });
+  }
+  try {
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: "User not Found" });
+    }
+
+    if (user.VerifyOtp === "" || user.VerifyOtp !== otp) {
+      return res.json({ success: false, message: "Invalid OTP" });
+    }
+
+    if (user.verifyOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: "OTP Expried " });
+    }
+
+    user.isAccountVerified = true;
+    user.VerifyOtp = "";
+    user.verifyOtpExpireAt = 0;
+
+    await user.save();
+    return res.json({ success: true, message: "Email Verified Successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
