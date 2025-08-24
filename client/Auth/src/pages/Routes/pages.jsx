25d4c954f24fc0b046edb2c6610cfc87ee1f4281
Naveen <naveenpokhrel99@gmail.com>
2025-08-24 15:27:30 +0545

    adding the routes

--- client/Auth/src/pages/Routes/pages.jsx
+++ client/Auth/src/pages/Routes/pages.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Home";
-
+import Login from "../Login";
+import EmailVerify from "../EmailVerify";
+import { ResetPassword } from "../ResetPassword";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
