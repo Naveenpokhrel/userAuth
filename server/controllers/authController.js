d158c25347ceeb51e5ab5573ac22978f228ce6b3
Naveen <naveenpokhrel99@gmail.com>
2025-08-25 16:24:28 +0545

    updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -79,9 +79,9 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: false,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax" ,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
--- server/server.js
+++ server/server.js
@@ -15,10 +15,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 ConnectDB();
+ConnectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(cors({ 
+    origin: "http://localhost:5173",
+    credentials: true }));
 
 
 // API ENDPoints
