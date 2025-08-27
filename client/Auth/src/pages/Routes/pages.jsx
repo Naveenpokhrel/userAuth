c4146ff06f219661251837c12ca31794dfdddf1d
Naveen <naveenpokhrel99@gmail.com>
2025-08-27 15:58:33 +0545

    updating the routes

--- client/Auth/src/pages/Routes/pages.jsx
+++ client/Auth/src/pages/Routes/pages.jsx
@@ -4,18 +4,23 @@ import Login from "../Login";
 import EmailVerify from "../EmailVerify";
 import ResetPassword from "../../pages/ResetPassword";
 import Signup from "../Register";
-import Dashboard from "../Dashboard";
+import { AppContentProvider } from "../../components/context/AppContext";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function AppRoutes() {
   return (
-    <BrowserRouter >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/email-verify" element={<EmailVerify />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+    <BrowserRouter>
+      <AppContentProvider>
+        <ToastContainer/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/email-verify" element={<EmailVerify />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </AppContentProvider>
     </BrowserRouter>
   );
 }
